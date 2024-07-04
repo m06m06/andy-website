221debca1c3516c440dd8cfae96c0cffcae17b1e
AndyChu24 <andy_chu@systex.com>
2024-07-04 15:54:23 +0800

    起點

---
+++ src/main/java/com/AndyChu/DevJourney/DevJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.AndyChu.DevJourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevJourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevJourneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AndyChu/DevJourney/DevJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AndyChu.DevJourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevJourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
